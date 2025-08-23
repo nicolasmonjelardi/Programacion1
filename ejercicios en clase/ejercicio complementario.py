5f6825e1c76bb72e16467a8beac8255aabcf5641
nicolasmonjelardi <nicolasmonjelardi@gmail.com>
2025-08-23 20:55:30 -0300

    Ejercicio complementario

---
+++ ejercicios en clase/ejercicio complementario.py
@@ -0,0 +1,80 @@
+#punto 1
+
+numero1=int(10)
+
+#punto 2
+
+numero2=float(5.2)
+
+#punto 3
+
+suma=float(numero1+numero2)
+
+#punto 4
+
+resta=float(numero1-numero2)
+multiplicacion=float(numero1*numero2)
+division=float(numero1/numero2)
+print(f"{numero1} + {numero2} = {suma}")
+print(f"{numero1} - {numero2} = {resta}")
+print(f"{numero1} X {numero2} = {multiplicacion}")
+print(f"{numero1} : {numero2} = {division}")
+
+#punto 5
+
+nombre="Nicolás"
+
+#punto 6
+
+precio=float(100.5)
+
+#punto 7
+
+descuento=float(0.5)
+
+#punto 8
+
+precio_final=float(precio-(precio*descuento))
+
+#punto 9
+
+cadena="hola mundo"
+
+#punto 10
+
+longitud=len(cadena)
+
+#punto 11
+
+precio=int(350.2)
+
+#punto 12
+
+nombre="Manuel "
+apellido="Branca"
+nombre_completo=nombre + apellido
+
+
+#punto 13
+
+edad=int(19)
+edad_mas_5=edad+5
+edad_menos_10=edad-10
+
+#punto 14
+
+altura=float(1.72)
+altura_x_4=float(altura*4)
+altura_dividido_3=float(altura/3)
+
+#punto 15
+
+nombre_mayusculas="NICOLAS"
+nombre_minusculas= nombre.lower()
+
+#punto 16
+
+nombre=nombre_mayusculas.lower().capitalize()
+print(f"{nombre}")
+
+
